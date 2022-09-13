33589658aff096ba3c701c5ce0bf834906235110
hoangtrongtin <hoangttin288@gmail.com>
2022-09-13 23:04:41 +0900

    add draft

--- algorithms.cpp
+++ algorithms.cpp
@@ -79,10 +79,14 @@ set<pair<lli, lli>, setcomp> setP;
 class UnionFind{
 public:
 	vector<int> p, rank;
+	vector<int> setSize;
+	int noSet;
 	UnionFind(int N){
 		rank.assign(N, 0);
 		p.assign(N, 0);
 		for(int i = 0; i < N; ++i) p[i] = i;
+		setSize.assign(N, 1);
+		noSet = N;
 	}
 	int findSet(int i){
 		return (p[i] == i) ? i : (p[i] = findSet(p[i]));
@@ -94,12 +98,23 @@ public:
 		if(!isSameSet(i, j)){
 			int x = findSet(i);
 			int y = findSet(j);
-			if(rank[x] > rank[y]) p[y] = x;
+			if(rank[x] > rank[y]){
+				p[y] = x;
+				// setSize[x] += setSize[y];
+			}
 			else{
 				p[x] = y;
 				if(rank[x] == rank[y]) rank[y]++;
+				// setSize[y] += setSize[x];
 			}
+			// --noSet;
 		}
+	}
+	int getNoSet(){
+		return noSet;
+	}
+	int getSetSize(int i){
+		return setSize[find(i)];
 	}
 };
 
